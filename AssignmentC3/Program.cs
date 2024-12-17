b7bfa66389715996c14aedd5f9a3e49b259db0ed
Amany Ahmed <eng.amanyahmed.81@gmail.com>
2024-12-17 11:33:11 +0200

    Add project files.

---
+++ AssignmentC3/Program.cs
@@ -0,0 +1,428 @@
+﻿using System;
+
+namespace AssignmentC3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Problem 01
+            //1- Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no.
+
+            //Console.Write("Enter a number: ");
+            //int num1 = int.Parse(Console.ReadLine());
+            //if (num1 % 3 == 0 && num1 % 4 == 0)
+            //{
+            //    Console.WriteLine("Yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No");
+            //}
+
+            #endregion
+
+
+            #region Problem 02
+
+            //2- Write a program that allows the user to insert an integer then print negative if it is negative number otherwise print positive.
+
+            //Console.Write("Enter an integer: ");
+            //int num2 = int.Parse(Console.ReadLine());
+
+            //switch (num2)
+            //{
+            //    case > 0:
+            //        Console.WriteLine("Positive");
+            //        break;
+            //    case < 0:
+            //        Console.WriteLine("Negative");
+            //        break;
+            //    default:
+            //        Console.WriteLine(value: "Equal !!");
+            //        break;
+            //}
+            #endregion
+
+
+            #region Problem 03
+
+            //3- Write a program that takes 3 integers from the user then prints the max element and the min element.
+
+            //Console.WriteLine("Enter three integers:");
+            //int num3 = int.Parse(Console.ReadLine());
+            //int num4 = int.Parse(Console.ReadLine());
+            //int num5 = int.Parse(Console.ReadLine());
+
+            //int max = Math.Max(Math.Max(num3, num4), num5);
+            //int min = Math.Min(Math.Min(num3, num4), num5);
+
+            //Console.WriteLine("Max element = " + max);
+            //Console.WriteLine("Min element = " + min);
+
+            #endregion
+
+
+            #region Problem 04
+
+            //4- Write a program that allows the user to insert an integer number then check If a number is even or odd.
+
+            //Console.Write("Enter an integer: ");
+            //int number = int.Parse(Console.ReadLine());
+
+            //if (number % 2 == 0)
+            //{
+            //    Console.WriteLine("Even");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Odd");
+            //}
+
+            #endregion
+
+
+            #region Problem 05
+
+            //5- Write a program that takes character from the user then if it is a vowel chars (a,e,I,o,u) then print (vowel) otherwise print (consonant).
+
+            //Console.Write("Enter a character: ");
+            //char ch = char.Parse(Console.ReadLine());
+
+            //if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            //{
+            //    Console.WriteLine("Vowel");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Consonant");
+            //}
+
+            #endregion
+
+
+            #region Problem 06
+
+            //6- Write a program that allows the user to insert an integer then print all numbers between 1 to that number.
+
+            //Console.Write("Enter a positive integer: ");
+            //int n = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    Console.Write(i + " ");
+            //}
+
+            #endregion
+
+
+            #region Problem 07
+
+            //7- Write a program that allows the user to insert an integer then print a multiplication table up to 12.
+
+            //Console.Write("Enter a number: ");
+            //int number = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= 12; i++)
+            //{
+            //    Console.WriteLine($"{number} x {i} = {number * i}");
+            //}
+
+            #endregion
+
+
+            #region Problem 08
+
+            //8- Write a program that allows to user to insert number then print all even numbers between 1 to this number
+
+            //Console.Write("Enter a positive integer: ");
+            //int n = int.Parse(Console.ReadLine());
+
+            //for (int i = 2; i <= n; i += 2)
+            //{
+            //    Console.Write(i + " ");
+            //}
+
+            #endregion
+
+
+            #region Problem 09
+
+            //9- Write a program that takes two integers then prints the power.
+
+            //Console.Write("Enter the base: ");
+            //int baseNum = int.Parse(Console.ReadLine());
+
+            //Console.Write("Enter the exponent: ");
+            //int exponent = int.Parse(Console.ReadLine());
+
+            //int result = 1;
+            //for (int i = 0; i < exponent; i++)
+            //{
+            //    result *= baseNum;
+            //}
+
+            //Console.WriteLine($"{baseNum}^{exponent} = {result}");
+
+            #endregion
+
+
+            #region Problem 10
+
+            //10- Write a program to enter marks of five subjects and calculate total, average and percentage.
+
+            //int[] marks = new int[5];
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.Write("Enter marks of subject {0}: ", i + 1);
+            //    marks[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //int total = 0;
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    total += marks[i];
+            //}
+
+            //double average = (double)total / 5;
+            //double percentage = (double)total / 500 * 100;
+
+            //Console.WriteLine("Total marks = " + total);
+            //Console.WriteLine("Average Marks = " + average);
+            //Console.WriteLine("Percentage = " + percentage);
+
+            #endregion
+
+
+            #region Problem 11
+
+            //11- Write a program to input the month number and print the number of days in that month.
+
+            //Console.Write("Enter month number: ");
+            //int month = int.Parse(Console.ReadLine());
+
+            //int days;
+
+            //switch (month)
+            //{
+            //    case 1:
+            //    case 3:
+            //    case 5:
+            //    case 7:
+            //    case 8:
+            //    case 10:
+            //    case 12:
+            //        days = 31;
+            //        break;
+            //    case 4:
+            //    case 6:
+            //    case 9:
+            //    case 11:
+            //        days = 30;
+            //        break;
+            //    case 2:
+            //        days = 28;
+            //        break;
+            //    default:
+            //        Console.WriteLine("Invalid month number");
+            //        return;
+            //}
+
+            //Console.WriteLine("Days in Month: " + days);
+
+            #endregion
+
+
+            #region Problem 12
+
+            //12- Write a program to create a Simple Calculator.
+
+            //Console.Write("Enter first number: ");
+            //double num1 = double.Parse(Console.ReadLine());
+
+            //Console.Write("Enter second number: ");
+            //double num2 = double.Parse(Console.ReadLine());
+
+            //Console.Write("Enter operator (+, -, *, /): ");
+            //char op = char.Parse(Console.ReadLine());
+
+            //double result;
+
+            //switch (op)
+            //{
+            //    case '+':
+            //        result = num1 + num2;
+            //        break;
+            //    case '-':
+            //        result = num1 - num2;
+            //        break;
+            //    case '*':
+            //        result = num1 * num2;
+            //        break;
+            //    case '/':
+            //        if (num2 == 0)
+            //        {
+            //            Console.WriteLine("Error: Division by zero");
+            //            return;
+            //        }
+            //        result = num1 / num2;
+            //        break;
+            //    default:
+            //        Console.WriteLine("Invalid operator");
+            //        return;
+            //}
+
+            //Console.WriteLine("Result: " + result);
+
+            #endregion
+
+
+            #region Problem 13
+
+            //13- Write a program to allow the user to enter a string and print the REVERSE of it.
+
+            //Console.Write("Enter a string: ");
+            //string str = Console.ReadLine();
+
+            //string reversedStr = "";
+            //for (int i = str.Length - 1; i >= 0; i--)
+            //{
+            //    reversedStr += str[i];
+            //}
+
+            //Console.WriteLine("Reversed string: " + reversedStr);
+
+            #endregion
+
+
+            #region Problem 14
+
+            //14- Write a program to allow the user to enter int and print the REVERSED of it.
+
+            //Console.Write("Enter an integer: ");
+            //int number = int.Parse(Console.ReadLine());
+
+            //int reversedNumber = 0;
+            //while (number != 0)
+            //{
+            //    int digit = number % 10;
+            //    reversedNumber = reversedNumber * 10 + digit;
+            //    number /= 10;
+            //}
+
+            //Console.WriteLine("Reversed number: " + reversedNumber);
+
+            #endregion
+
+
+            #region Problem 15
+
+            //15- Write a program in C# Sharp to find prime numbers within a range of numbers.
+
+            //Console.Write("Enter starting number of range: ");
+            //int start = int.Parse(Console.ReadLine());
+
+            //Console.Write("Enter ending number of range: ");
+            //int end = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Prime numbers between {0} and {1}:", start, end);
+            //for (int num = start; num <= end; num++)
+            //{
+            //    bool isPrime = true;
+            //    for (int i = 2; i <= Math.Sqrt(num); i++)
+            //    {
+            //        if (num % i == 0)
+            //        {
+            //            isPrime = false;
+            //            break;
+            //        }
+            //    }
+            //    if (isPrime && num > 1)
+            //    {
+            //        Console.Write(num + " ");
+            //    }
+            //}
+
+
+            #endregion
+
+
+            #region Problem 16
+
+            //16- Write a program in C# Sharp to convert a decimal number into binary without using an array.
+
+            //Console.Write("Enter a number to convert: ");
+            //int number = int.Parse(Console.ReadLine());
+
+            //string binary = "";
+            //while (number > 0)
+            //{
+            //    binary = number % 2 + binary;
+            //    number /= 2;
+            //}
+
+            //Console.WriteLine("The Binary of {0} is {1}", number, binary);
+
+            #endregion
+
+
+            #region Problem 17
+
+            //17- Create a program that asks the user to input three points (x1, y1), (x2, y2), and (x3, y3), and determines whether these points lie on a single straight line.
+
+            //Console.WriteLine("Enter coordinates of three points (x1, y1), (x2, y2), (x3, y3):");
+
+            //int x1 = int.Parse(Console.ReadLine());
+            //int y1 = int.Parse(Console.ReadLine());
+            //int x2 = int.Parse(Console.ReadLine());
+            //int y2 = int.Parse(Console.ReadLine());
+            //int x3 = int.Parse(Console.ReadLine());
+            //int y3 = int.Parse(Console.ReadLine());
+
+            //double area = Math.Abs((x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2.0);
+
+            //Console.WriteLine("Area of the triangle: " + area);
+
+            #endregion
+
+
+            #region Problem 18
+
+            //18- Within a company, the efficiency of workers is evaluated based on the duration required to complete a specific task. A worker's efficiency level is determined as follows: 
+            /* -If the worker completes the job within 2 to 3 hours, they are considered highly efficient.
+            - If the worker takes 3 to 4 hours, they are instructed to increase their speed.
+            - If the worker takes 4 to 5 hours, they are provided with training to enhance their speed.
+            - If the worker takes more than 5 hours, they are required to leave the company.
+              To calculate the efficiency of a worker, the time taken for the task is obtained via user input from the keyboard.*/
+
+            //Console.Write("Enter the time taken to complete the task (in hours): ");
+            //double timeTaken = double.Parse(Console.ReadLine());
+
+            //if (timeTaken >= 2 && timeTaken < 3)
+            //{
+            //    Console.WriteLine("Highly Efficient Worker!");
+            //}
+            //else if (timeTaken >= 3 && timeTaken < 4)
+            //{
+            //    Console.WriteLine("Please increase your speed.");
+            //}
+            //else if (timeTaken >= 4 && timeTaken < 5)
+            //{
+            //    Console.WriteLine("Training is required to enhance your speed.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You are required to leave the company.");
+            //}
+
+            #endregion
+
+
+
+
+
+        }
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,448 @@
+﻿using System;
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            #region String Formating
+            // String Formating
+            // Equation ; X +Y =12
+
+            //int X = 3, Y = 4;
+
+            // 1. string Cocatnation
+
+            //String Message = "Equaion : " + X + "+" + Y + "=" + (X + Y);
+
+            //Console.WriteLine(Message);
+            // string : Immutable -> Can't Change its Value After Creation
+
+            // 2. String Format
+
+            //string Message = string.Format(format: "Euation : {0} + {1} = {2}", X, Y, X + Y);
+            //Console.WriteLine(Message);
+
+            // 3. String Interpolution
+            // $ :
+
+            //string Message = $"Equation ; {X} +{Y} ={X + Y}";
+            //Console.WriteLine(Message); 
+            #endregion
+
+            #region Control Statements
+
+            // Control Statements
+            // Control Statements : 1. Conditional Controle Statments (If Statment, Switch Case)
+            // Control Statements : 2. Loop Control Statements (For, While, do-While)
+
+            //Console.WriteLine(value:"Hello, World!");
+            //Console.WriteLine(value: "Hello, World!");
+            //Console.WriteLine(value: "Hello, World!");
+            //Console.WriteLine(value: "Hello, World!");
+
+
+
+
+            #endregion
+
+            #region Control Statements : 1. Conditional Controle Statments (If Statment, Switch Case)
+
+
+            //Console.WriteLine(value: "Enter Number of Month in 1st Quarter : ");
+            //int MonthNumber = int.Parse(Console.ReadLine());
+
+            // 1 --> Jan
+            // 2 --> Feb
+            // 3 --> Mar
+
+            //Console.WriteLine(value: "Jan");
+            //Console.WriteLine(value: "Feb");
+            //Console.WriteLine(value: "Mar");
+
+            //if(/*Expression*/)
+            //{
+            //    // code
+            //    // This Code will be executed when the condition is true
+            //}
+
+            //if (MonthNumber == 1) 
+            //{
+            //    // code
+            //    Console.WriteLine(value: "Jan");
+            //}
+
+            #endregion
+
+            #region Ex01
+
+            //Console.WriteLine(value: "Enter Number of Month in 1st Quarter : ");
+            //int MonthNumber = int.Parse(Console.ReadLine());
+
+            //if (MonthNumber == 2)
+            //{
+            //    // code
+            //    Console.WriteLine(value: "Feb");
+            //}
+
+            //if (MonthNumber == 3)
+            //{
+            //    // code
+            //    Console.WriteLine(value: "Mar");
+            //}
+
+            //if(MonthNumber == 1)
+            //{
+            //    Console.WriteLine(value: "Jan");
+            //}else if(MonthNumber == 2)
+            //{
+            //    Console.WriteLine(value: "Feb");
+            //}else if(MonthNumber == 3)
+            //{
+            //    Console.WriteLine(value: "Mar");
+            //}else
+            //{
+            //    Console.WriteLine(value: "Invalid Month Number");
+            //}
+
+            // 2. using Switch 
+
+            //switch(/*Expression*/)
+            //{
+
+            //}
+
+            //Console.WriteLine(value: "Enter Number of Month in 1st Quarter : ");
+            //int MonthNumber = int.Parse(Console.ReadLine());
+
+
+            //// Jump Table
+            //switch (MonthNumber)
+            //{
+            //    case 1:
+            //        Console.WriteLine(value: "Jan");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine(value: "Feb");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine(value: "Mar");
+            //        break;
+            //    default:
+            //        Console.WriteLine(value: "Invalid Month Number");
+            //        break;
+            //}
+            #endregion
+
+            #region Ex02
+
+            //Console.WriteLine(value: "Enter Your Name: ");
+            //string Name = Console.Readline();
+
+            // Hi Ahmed
+
+            //if(Name == "Ahmed" || Name == "ahmed")
+            //{
+            //    Console.WriteLine(value: "Hi Ahmed");
+            //}else if(Name == "Omar" || Name == "omar")
+            //{
+            //    Console.WriteLine(value: "Hi Omar");
+            //}else if(Name == "Ali" || Name == "ali")
+            //{
+            //    Console.WriteLine(value: "Hi Ali");
+            //}
+            //else if (Name == "Mariam" || Name == "mariam")
+            //{
+            //    Console.WriteLine(value: "Hi Mariam");
+            //}
+
+
+
+            //Console.WriteLine(value: "Enter Your Name: ");
+            //string Name = Console.ReadLine();
+
+            //// Jump Table
+            //switch (Name)
+            //{
+            //    case "Ahmed":
+            //    case "ahmed":
+            //        Console.WriteLine(value: "Hi Ahmed");
+            //        break;
+            //    case "Omar":
+            //    case "omar":
+            //        Console.WriteLine(value: "Hi Omar");
+            //        break;
+            //    case "Ali":
+            //    case "ali":
+            //        Console.WriteLine(value: "Hi Ali");
+            //        break;
+            //    case "Mariam":
+            //    case "mariam":
+            //        Console.WriteLine(value: "Hi Mariam");
+            //        break;
+            //    default:
+            //        Console.WriteLine(value: "Invalid Name");
+            //        break;
+            //}
+
+            #endregion
+
+            #region Ex03
+
+            Console.WriteLine(value: "Enter Your Age : ");
+            int Age = int.Parse(Console.ReadLine());
+
+            // 1. Using If
+
+            //if (Age > 18)
+            //{
+            //    Console.WriteLine(value: "Welcom :)");
+            //}
+            //else if (Age < 18)
+            //{
+            //    Console.WriteLine(value: "Bye Bye :(");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(value: "Equal !!");
+            //}
+
+            //switch (Age)
+            //{
+            //    case > 18:
+            //        Console.WriteLine(value: "Welcom :)");
+            //        break;
+            //    case < 18:
+            //        Console.WriteLine(value: "Bye Bye :(");
+            //        break;
+            //    default:
+            //        Console.WriteLine(value: "Equal !!");
+            //        break;
+            //}
+            #endregion
+
+            #region C# 7.0
+            //object object = new object();
+
+            //object = "Ahmed";
+            //object = 1;
+
+            //switch (object)
+            //{
+            //    case int X when X > -12:
+            //        Console.WriteLine(value: "Int object");
+            //        break;
+            //    case string X when X.Length > 5:
+            //        Console.WriteLine(value: "string object");
+            //        break;
+            //    case string X when X.Length <= 5:
+            //        Console.WriteLine(value: "string object <= 5");
+            //        break;
+
+            #endregion
+
+            #region c# 8.0
+
+            //string Input = Console.ReadLine();
+            // 1
+            // 2
+            // 3
+
+            //Console.WriteLine(Input switch
+            //{
+            //    "1" => "option 1",
+            //    "2" => "option 2",
+            //    "3" => "option 3",
+            //    _ => "Unsupport Option ! !"
+            //});
+
+            #endregion
+
+            #region  Control Statements : 2. Loop Control Statements (For, foreach)
+
+            // 1. for loop
+
+            //for(/*Statment*/;/* Expression*/;/*Statment*/  )
+            //{
+            //    // code
+            //    // That Code Will Be Repeated
+            //}
+
+            //for(iConsole.WriteLine(value: "Hello 1"); Console.WriteLine(value: "Hello 2"))
+            //{
+            //    Console.WriteLine(value:"Hello Worled");
+            //    i++;
+            //}
+
+
+            //for(intX = 1; X <= 10; X++)
+            //{
+            //     Console.WritLine(X);
+            //}
+
+
+            //2. foreach
+
+            //int[] Numbers  = { 1, 2, 3, 4, 5, 6, 7}
+
+            //for(int i=0; i < Numbers.Length; i++)
+            //{
+
+            //    Console.WriteLine(Numbers[i]);
+            //}
+
+            //foreach(int N in Numbers)
+            //{
+            //Console.WriteLine(N);
+            //}
+
+            #endregion
+
+            #region Control Statements : 2. Loop Control Statements (While, do-While)
+
+            // while (true)
+            //{
+            // Console.WriteLine("Hallo Worled");
+            //}
+
+            //int i = 1;
+
+            //while (i <= 10)
+            //{
+            //    Console.WriteLine(value:"Hello Worled");
+            //    i++;
+            //}
+
+            //Console.WriteLine(i);
+
+            // do - while
+
+            //do
+            //{
+            //    //code
+            //    // This Code Will Be Executed First
+            //} while (/* Expression*/);
+
+
+            //int i = 11;
+
+            //do
+            //{
+            //    Console.WriteLine(value: "Hello Worled");
+            //} while (i <= 10);
+
+            //int Number;
+            //bool Flag;
+            //do
+            //{
+            //    Console.WriteLine(value: "Even Number :");
+            //} while (!int.TryParse(Console.ReadLine(), out Number) == false || Number % 2 != 0);
+
+            //Console.WriteLine(Number);
+
+            #endregion
+
+            #region string
+            //// string : Reference Type : HEAP (Class)
+            //// Immutable Type : Can't Change its Value After Creation
+            //string S01;
+            ////Declare For Reference (Pointer) From Type 'String'
+            //// S01 : Can Refer To Object From 'String'
+            //// S01 : Refer To Null
+
+            //// 8 Byte Will Be Allocated At Stack For Refernce 'S01'
+            //// 0 Byte Will Be Allocated At HEAP
+
+
+            //// S01 = new string(value: "Ahmed");
+            //S01 = "Ahmed";  // Syntax Sugar
+            //                // new
+            //                // 1. Allocate The Number Of Required Bytes for The Object At HEAP
+            //                // 2. Initialized The Allocated Bytes With The Default Value
+            //                // 3. Call User-Defined Constructor is Exists
+            //                // 4. Assign The Object To The Reference
+            //Console.WriteLine(S01.GetHashCode()); //Id : Indicate Address
+
+
+            //S01 = "Ali";
+            //// 1. Allocate The Number Of Required Bytes for The Object At HEAP
+            //// 2. Initialized The Allocated Bytes With The Default Value
+            //// 3. Call User-Defined Constructor is Exists
+            //// 4. Assign The Object To The Reference
+            //Console.WriteLine(S01.GetHashCode()); //Id : Indicate Address
+
+
+            //S01 = "Yassmin";
+            //// new
+            //// 1. Allocate The Number Of Required Bytes for The Object At HEAP
+            //// 2. Initialized The Allocated Bytes With The Default Value
+            //// 3. Call User-Defined Constructor is Exists
+            //// 4. Assign The Object To The Reference
+            //Console.WriteLine(S01.GetHashCode()); //Id : Indicate Address
+
+            //string Message = "Hello";
+
+            //Console.WriteLine("Message :" + Message);
+            //Console.WriteLine("HashCode :" + Message.GetHashCode());
+            //Console.WriteLine();
+
+            //Message += "Ahmed";
+
+            //Console.WriteLine("Message :" + Message);
+            //Console.WriteLine("HashCode :" + Message.GetHashCode());
+            //Console.WriteLine(); 
+            #endregion
+
+            #region String Builder
+            // StringBuilder: Class (Reference Type) : HEAP
+            // Mutable Datatype : Can Change its Value After Creation
+
+
+            //StringBuilder S01;
+            //////Declare For Reference (Pointer) From Type 'StringBuilder'
+            ////// S01 : Can Refer To Object From 'StringBuilder'
+            ////// S01 : Refer To Null
+
+            //S01 = new string(value: "Ahmed");
+            ///// new
+            //// 1. Allocate The Number Of Required Bytes for The Object At HEAP
+            //// 2. Initialized The Allocated Bytes With The Default Value
+            //// 3. Call User-Defined Constructor is Exists
+            //// 4. Assign The Object To The Reference
+            //Console.WriteLine(S01.GetHashCode()); //Id : Indicate Address
+
+            //Console.WriteLine("S01 :" + S01);
+            //Console.WriteLine("S01HashCode :" + S01.GetHashCode());
+            //Console.WriteLine();
+
+            //// S01 = new StringBuilder(value: "Ahmed");
+
+            //S01.Append(value: "Ahmed");
+
+            //Console.WriteLine("S01 :" + S01);
+            //Console.WriteLine("S01HashCode :" + S01.GetHashCode());
+            //Console.WriteLine();
+
+            #endregion
+
+            #region String Methods
+
+            #endregion
+
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+        
+}
